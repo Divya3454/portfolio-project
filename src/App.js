500cca5357f3a93abbb9ae7d79a370adb8779211
Divya Sree kandi <user@Joes-MBP.home>
2021-05-03 13:41:55 -0500

    Portfolio using react js and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import  './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Particles from "react-particles-js";
+import Navbar from "./components/Navbar.js";
+import Header from "./components/Header";
 function App() {
   return (
-   <h2> Portfolio Project ...</h2>
+    <>
+    <Particles 
+    className="particles-canvas"
+    params={{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value: 900
+          }
+        },
+        shape:{
+          type: "circle",
+         stroke: {
+           width: 6,
+           color: "#ef4035"
+         } 
+        }
+      }
+    }}
+    />
+   <Navbar/>
+   <Header/>
+   </>
   );
 }
 
